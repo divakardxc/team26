6c460441b758d6a1bf62abc89273ce5da618eb90
Divakar Bhat <divakar.bhat-dxc@hpe.com>
2023-07-01 18:21:38 +0530

    Initial Commit with Spring Boot

---
+++ Online Education Platform/src/test/java/com/online/platform/demo/EducationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.platform.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
